a71ab901b6a981cd4bbc21abdfd620304453cc1f
Alex <strong.daronin@gmail.com>
2025-11-17 05:47:35 +0100

    Initial Commit

---
+++ FinanceFlow/FinanceFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceFlowApp.swift
+//  FinanceFlow
+//
+//  Created by Aleksey Daronin on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
